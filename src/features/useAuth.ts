2fe6fba550b795421ec07ac7d11953db6c4af3fb
Saurabh Soni <sonisaurabhsoni333@gmail.com>
2023-12-13 15:29:45 +0530

    commit

--- src/features/useAuth.ts
+++ src/features/useAuth.ts
@@ -1,4 +1,4 @@
-import { useSelector } from "react-redux";
+// import { useSelector } from "react-redux";
 
-const user = useSelector((state: any) => state.auth.value.isAuth)
-export default user
+// const user = useSelector((state: any) => state.auth.value.isAuth)
+// export default user
