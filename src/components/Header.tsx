216a4452176ccfe2265d04637f8fa34e070e6668
Saurabh Soni <sonisaurabhsoni333@gmail.com>
2023-12-26 09:23:47 +0530

    commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -211,74 +211,86 @@ export default function Header() {
                 isDismissable={false}
                 size="5xl"
                 scrollBehavior="outside"
+                placement="center"
             >
                 <ModalContent>
                     {(onClose) => (
                         <>
                             <ModalHeader className="flex flex-col gap-1">Register Your Shop</ModalHeader>
                             <ModalBody >
-                                <form className="flex flex-col gap-1">
-                                    <section className="flex flex-wrap gap-1">
-                                        <Input autoFocus
-                                            isRequired
-                                            className="max-w-xs"
-                                            label="Shop Name"
-                                            variant="flat" />
-                                        <Select className="max-w-xs" isRequired label="Choose shop category">
-                                            {
-                                                shopCategories.map((category) => {
-                                                    return <SelectItem value={category.name} key={category.id}>{category.name}</SelectItem>
-                                                })
-                                            }
-                                        </Select>
+                                <form>
+                                    <Accordion variant="bordered" defaultExpandedKeys={["shop details"]}>
+                                        <AccordionItem
+                                            key={"shop details"}
+                                            title={"Shop Details"}
+                                        >
+                                            <section className="flex flex-wrap gap-1 justify-center items-center">
+                                                <Input
+                                                    isRequired
+                                                    className="max-w-md"
+                                                    label="Shop Name"
+                                                    variant="flat" />
+                                                <Select className="max-w-md" isRequired label="Choose shop category">
+                                                    {
+                                                        shopCategories.map((category) => {
+                                                            return <SelectItem value={category.name} key={category.id}>{category.name}</SelectItem>
+                                                        })
+                                                    }
+                                                </Select>
 
-                                        <Input className="max-w-xs" label="Shop Address" isRequired variant="flat" />
-                                        <Input className="max-w-xs" label="Shop Description" isRequired variant="flat" />
-                                        <Input className="max-w-xs" type="number" isRequired label="Shop Phone" variant="flat" />
-                                        <Input className="max-w-xs" label="Shop Email" variant="flat" />
-                                        <Input className="max-w-xs" label="GSTIN Number" description="USE 22AAAAA0000A1Z5 FORMAT" isRequired variant="flat" />
-                                        <Input className="max-w-xs" label="PAN Number" description="USE AAAAA0000A FORMAT" isRequired variant="flat" />
-                                        <Input className="max-w-xs" label="Aadhar Number" description="USE 0000 0000 0000 FORMAT" isRequired variant="flat" />
-                                        <Accordion isCompact variant="bordered">
-                                            <AccordionItem key={"bank details"} title={"Bank Details"}>
-                                                <section className="flex flex-wrap gap-1">
+                                                <Input className="max-w-md" label="Shop Address" isRequired variant="flat" />
+                                                <Input className="max-w-md" label="Shop Description" isRequired variant="flat" />
+                                                <Input className="max-w-md" type="number" isRequired label="Shop Phone" variant="flat" />
+                                                <Input className="max-w-md" label="Shop Email" variant="flat" />
+                                                {/* <Divider /> */}
+                                                <Accordion variant="bordered" defaultExpandedKeys={"important details"}>
+                                                    <AccordionItem key={"important details"} title={"Important Details"}>
+                                                        <Input className="max-w-full" label="GSTIN Number" description="USE 22AAAAA0000A1Z5 FORMAT" isRequired variant="flat" />
+                                                        <Input className="max-w-full" label="PAN Number" description="USE AAAAA0000A FORMAT" isRequired variant="flat" />
+                                                        <Input className="max-w-full" label="Aadhar Number" description="USE 0000 0000 0000 FORMAT" isRequired variant="flat" />
+                                                    </AccordionItem>
+                                                </Accordion>
+                                            </section>
+                                        </AccordionItem>
+                                        <AccordionItem key={"bank details"} title={"Bank Details"}>
+                                            <section className="flex flex-wrap gap-1">
 
-                                                    <Input className="max-w-xs" label="Bank Account Number" description="USE 0000 0000 0000 FORMAT" isRequired variant="flat" />
-                                                    <Input className="max-w-xs" label="IFSC Code" description="USE 0000 0000 0000 FORMAT" onChange={() => {
-                                                    }} isRequired variant="flat" />
-                                                    <Input className="max-w-xs" label="Bank Name" isDisabled variant="flat" />
-                                                    <Input className="max-w-xs" label="Bank Branch" isDisabled variant="flat" />
-                                                    <Input className="max-w-xs" label="Bank Address" isDisabled variant="flat" />
-                                                    <Input className="max-w-xs" label="Bank City" isDisabled variant="flat" />
-                                                    <Select className="max-w-xs" disabled label="Choose bank state">
-                                                        {
-                                                            bankstates.states.map((category) => {
-                                                                return <SelectItem value={category.name} key={category.id}>{category.name}</SelectItem>
-                                                            })
-                                                        }
-                                                    </Select>
-                                                    <Input className="max-w-xs" label="Bank Pincode" isDisabled variant="flat" />
-                                                    <Input className="max-w-xs" label="Bank Phone" isDisabled variant="flat" />
-                                                    <Input className="max-w-xs" label="Bank Email" isDisabled variant="flat" />
-                                                    <Input className="max-w-xs" label="Bank Website" isDisabled variant="flat" />
-                                                </section>
-                                            </AccordionItem>
+                                                <Input className="max-w-md" label="Bank Account Number" description="USE 0000 0000 0000 FORMAT" isRequired variant="flat" />
+                                                <Input className="max-w-md" label="IFSC Code" description="USE 0000 0000 0000 FORMAT" onChange={() => {
+                                                }} isRequired variant="flat" />
+                                                <Input className="max-w-md" label="Bank Name" isDisabled variant="flat" />
+                                                <Input className="max-w-md" label="Bank Branch" isDisabled variant="flat" />
+                                                <Input className="max-w-md" label="Bank Address" isDisabled variant="flat" />
+                                                <Input className="max-w-md" label="Bank City" isDisabled variant="flat" />
+                                                <Select className="max-w-md" disabled label="Choose bank state">
+                                                    {
+                                                        bankstates.states.map((category) => {
+                                                            return <SelectItem value={category.name} key={category.id}>{category.name}</SelectItem>
+                                                        })
+                                                    }
+                                                </Select>
+                                                <Input className="max-w-md" label="Bank Pincode" isDisabled variant="flat" />
+                                                <Input className="max-w-md" label="Bank Phone" isDisabled variant="flat" />
+                                                <Input className="max-w-md" label="Bank Email" isDisabled variant="flat" />
+                                                <Input className="max-w-md" label="Bank Website" isDisabled variant="flat" />
+                                            </section>
+                                        </AccordionItem>
 
-                                            <AccordionItem
-                                                key={"social media"}
-                                                title="Social Media(Not Required)"
-                                            >
-                                                <section className="flex flex-wrap gap-1">
-                                                    <Input className="max-w-xs" type="url" label="Shop Website" variant="flat" />
-                                                    <Input className="max-w-xs" type="url" label="Shop Facebook" variant="flat" />
-                                                    <Input className="max-w-xs" type="url" label="Shop Twitter" variant="flat" />
-                                                    <Input className="max-w-xs" type="url" label="Shop Instagram" variant="flat" />
-                                                    <Input className="max-w-xs" type="url" label="Shop Youtube" variant="flat" />
-                                                    <Input className="max-w-xs" type="url" label="Shop Whatsapp" variant="flat" />
-                                                </section>
-                                            </AccordionItem>
-                                        </Accordion>
-                                    </section>
+                                        <AccordionItem
+                                            key={"social media"}
+                                            title="Social Media(Not Required)"
+                                        >
+                                            <section className="flex flex-wrap gap-1">
+                                                <Input className="max-w-md" type="url" label="Shop Website" variant="flat" />
+                                                <Input className="max-w-md" type="url" label="Shop Facebook" variant="flat" />
+                                                <Input className="max-w-md" type="url" label="Shop Twitter" variant="flat" />
+                                                <Input className="max-w-md" type="url" label="Shop Instagram" variant="flat" />
+                                                <Input className="max-w-md" type="url" label="Shop Youtube" variant="flat" />
+                                                <Input className="max-w-md" type="url" label="Shop Whatsapp" variant="flat" />
+                                            </section>
+                                        </AccordionItem>
+                                    </Accordion>
+
                                 </form>
                             </ModalBody>
                             <ModalFooter>
